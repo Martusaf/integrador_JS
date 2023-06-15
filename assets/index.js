a87a1b6ba0a474674cd214e0eff9905ec27957dc
Martusaf <safigueroamartinarocio@gmail.com>
2023-06-15 00:18:56 -0300

    primer js

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,37 @@
+const productContainer = document.querySelector (".product-container");
 
+
+
+
+
+const productSetting = (product) => {
+const {id, name, price, cardImg} = product;
+    return `
+    <div class="product">
+        <img src=${cardImg} alt=${name}/>
+        <h3>${name}</h3>
+        <p>${price}$</p>
+        <button class="btn" data-id="${id}" data-name="${name}" data-price="${price}" data-img="${cardImg}">
+        add to cart
+        </button>
+    </div>
+  `
+}
+
+
+
+const renderProducts = (productList) => {
+    productContainer.innerHTML = productList.map(productSetting).join("");
+};
+
+
+
+
+
+
+
+const init = () => {
+    renderProducts(productInfo);
+};
+
+init();
